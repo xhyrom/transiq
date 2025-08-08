2e6567f18e7407227bb0e0abf02f97ee6be22e15
Jozef Steinhübl <contact@xhyrom.dev>
2025-08-08 07:51:26 +0200

    ci: update view

--- gtfs-view/index.mjs
+++ gtfs-view/index.mjs
@@ -10,10 +10,8 @@ const gtfs = await Array.fromAsync(glob(join("gtfs", "**/*.zip")));
 
 await mkdir(join(".tmp", "tidy-gtfs"), { recursive: true });
 
-const tidyGtfs = [];
-
 for (const gtf of gtfs) {
-  if (gtf.includes("dpb")) continue; // too large, skip for now
+  if (gtf.includes("dpb")) continue;
 
   const agencyKey = gtf
     .split("/")
@@ -24,64 +22,64 @@ for (const gtf of gtfs) {
   console.log(`Tidy GTFS: ${gtf} -> ${tidyGtfPath}`);
   await exec(`gtfstidy -SCRmTcdsOeD ${gtf} -o ${tidyGtfPath}`);
 
-  tidyGtfs.push({ agencyKey, path: tidyGtfPath });
+  await gtfsToHtml({
+    agencies: [
+      {
+        agencyKey: agencyKey,
+        path: tidyGtfPath,
+      },
+    ],
+    allowEmptyTimetables: false,
+    beautify: false,
+    coordinatePrecision: 5,
+    dateFormat: "MMM D, YYYY",
+    daysShortStrings: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    daysStrings: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+    defaultOrientation: "vertical",
+    interpolatedStopSymbol: "•",
+    interpolatedStopText: "Estimated time of arrival",
+    linkStopUrls: false,
+    mapStyleUrl: "https://tiles.openfreemap.org/styles/liberty",
+    menuType: "jump",
+    noDropoffSymbol: "‡",
+    noDropoffText: "No drop off available",
+    noHead: false,
+    noPickupSymbol: "**",
+    noPickupText: "No pickup available",
+    noServiceSymbol: "—",
+    noServiceText: "No service at this stop",
+    outputFormat: "html",
+    overwriteExistingFiles: true,
+    outputPath: `.tmp/build/gtfs-view/${agencyKey}`,
+    requestDropoffSymbol: "†",
+    requestDropoffText: "Must request drop off",
+    requestPickupSymbol: "***",
+    requestPickupText: "Request stop - call for pickup",
+    serviceNotProvidedOnText: "Service not provided on",
+    serviceProvidedOnText: "Service provided on",
+    showArrivalOnDifference: 0.2,
+    showCalendarExceptions: true,
+    showDuplicateTrips: false,
+    showMap: true,
+    showOnlyTimepoint: true,
+    showRouteTitle: true,
+    showStopCity: true,
+    showStopDescription: true,
+    showStoptimesForRequestStops: true,
+    skipImport: false,
+    sortingAlgorithm: "common",
+    timeFormat: "h:mma",
+    useParentStation: true,
+    ignoreDuplicates: true,
+    verbose: true,
+    zipOutput: false,
+  });
 }
-
-await gtfsToHtml({
-  agencies: tidyGtfs.map((gtf) => ({
-    agencyKey: gtf.agencyKey,
-    path: gtf.path,
-  })),
-  allowEmptyTimetables: false,
-  beautify: false,
-  coordinatePrecision: 5,
-  dateFormat: "MMM D, YYYY",
-  daysShortStrings: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-  daysStrings: [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ],
-  defaultOrientation: "vertical",
-  interpolatedStopSymbol: "•",
-  interpolatedStopText: "Estimated time of arrival",
-  linkStopUrls: false,
-  mapStyleUrl: "https://tiles.openfreemap.org/styles/liberty",
-  menuType: "jump",
-  noDropoffSymbol: "‡",
-  noDropoffText: "No drop off available",
-  noHead: false,
-  noPickupSymbol: "**",
-  noPickupText: "No pickup available",
-  noServiceSymbol: "—",
-  noServiceText: "No service at this stop",
-  outputFormat: "html",
-  overwriteExistingFiles: true,
-  outputPath: ".tmp/build/gtfs-view",
-  requestDropoffSymbol: "†",
-  requestDropoffText: "Must request drop off",
-  requestPickupSymbol: "***",
-  requestPickupText: "Request stop - call for pickup",
-  serviceNotProvidedOnText: "Service not provided on",
-  serviceProvidedOnText: "Service provided on",
-  showArrivalOnDifference: 0.2,
-  showCalendarExceptions: true,
-  showDuplicateTrips: false,
-  showMap: true,
-  showOnlyTimepoint: true,
-  showRouteTitle: true,
-  showStopCity: true,
-  showStopDescription: true,
-  showStoptimesForRequestStops: true,
-  skipImport: false,
-  sortingAlgorithm: "common",
-  timeFormat: "h:mma",
-  useParentStation: true,
-  ignoreDuplicates: true,
-  verbose: true,
-  zipOutput: false,
-});
