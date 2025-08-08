74f9ca1af712958633dc083beb64fc4544a97128
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2025-08-08 09:24:07 +0200

    fix(gtfs-ciew): exclude dpb for now

--- gtfs-view/index.mjs
+++ gtfs-view/index.mjs
@@ -11,6 +11,8 @@ const gtfs = await Array.fromAsync(glob(join("gtfs", "**/*.zip")));
 await mkdir(join(".tmp", "tidy-gtfs"), { recursive: true });
 
 for (const gtf of gtfs) {
+  if (gtf.includes("dpb")) continue;
+  
   const agencyKey = gtf
     .split("/")
     .pop()
