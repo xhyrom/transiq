bc63c2b06c58921db54c4959f91ee88aed6be7e0
Jozef Steinhübl <contact@xhyrom.dev>
2025-08-12 07:12:07 +0200

    fix: add missing agency urls

--- feeds/sk/sad-humenne-as.ts
+++ feeds/sk/sad-humenne-as.ts
@@ -7,6 +7,20 @@ export default {
     notes:
       "Data sourced from portal.cp.sk, provided by SAD Humenné a.s. Automatically converted from XLSX to GTFS format.",
   },
+  fixes: {
+    "agency.txt": {
+      updateRows: [
+        {
+          where: {
+            agency_name: "SAD Humenné a.s.",
+          },
+          set: {
+            agency_url: "https://www.sadhe.sk/",
+          },
+        },
+      ],
+    },
+  },
   getLatestSource: async (): Promise<FileSource> => ({
     type: "cis",
     id: "sad-humenne-as",
--- feeds/sk/sad-poprad-as.ts
+++ feeds/sk/sad-poprad-as.ts
@@ -7,6 +7,20 @@ export default {
     notes:
       "Data sourced from portal.cp.sk, provided by SAD Poprad a.s. Automatically converted from XLSX to GTFS format.",
   },
+  fixes: {
+    "agency.txt": {
+      updateRows: [
+        {
+          where: {
+            agency_name: "SAD Poprad a.s.",
+          },
+          set: {
+            agency_url: "https://sadpp.sk/",
+          },
+        },
+      ],
+    },
+  },
   getLatestSource: async (): Promise<FileSource> => ({
     type: "cis",
     id: "sad-poprad-as",
