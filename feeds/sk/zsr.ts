85f7b703f765d57c3f3c54c301c5f9edc7ad8ad4
Jozef Steinhübl <contact@xhyrom.dev>
2026-05-23 10:26:29 +0200

    feat: zsr

--- feeds/sk/zsr.ts
+++ feeds/sk/zsr.ts
@@ -1,17 +1,17 @@
 import type { Feed, FileSource } from "@feeds/types";
 
 export default {
-  id: "zssk",
-  name: "Železničná spoločnosť Slovensko, a.s.",
+  id: "zsr",
+  name: "Železničná spoločnosť Slovensko, a.s. ; RegioJet, a.s. ; Leo Express s.r.o. ; Leo Express Slovensko s.r.o. ; LTE Logistik a Transport Slovakia, s.r.o. ; Trenčianska elektrická železnica, n.o.",
   license: {
     type: "CC0-1.0",
     url: "https://creativecommons.org/publicdomain/zero/1.0/deed.en",
     attribution: "© Železnice Slovenskej republiky (ŽSR)",
     notes: "Data provided by Železnice Slovenskej republiky",
-    from: "https://data.slovensko.sk/datasety/ca4cb74c-7192-4198-b074-34acd9d295e7",
+    from: "https://data.slovensko.sk/datasety/ebeeedf1-aca2-451a-bdc0-35d536714888",
   },
   getLatestSource: async (): Promise<FileSource> => ({
     type: "url",
-    url: "https://www.zsr.sk/files/pre-cestujucich/cestovny-poriadok/gtfs/gtfs.zip",
+    url: "https://data.slovensko.sk/download?id=c5a63281-3c44-4dba-82c9-7b7ad603db5d",
   }),
 } satisfies Feed;
